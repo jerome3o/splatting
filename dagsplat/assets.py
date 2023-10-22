daf139fa5df901aa11095d36cc255cd879ab4401
Jerome Swannack <jeromeswannack@gmail.com>
2023-10-22 15:58:42 +1300

    Added point_cloud asset

--- dagsplat/assets.py
+++ dagsplat/assets.py
@@ -1,3 +1,4 @@
+import sys
 import tempfile
 from pathlib import Path
 from pydantic import Field
@@ -17,6 +18,7 @@ _IMAGE_FILE_EXTENSIONS = ["jpg", "jpeg", "png"]
 class GaussianSplatConfig(ConfigurableResource):
     input_dir: str = "data/splat_inputs/bedroom"
     root_dir: str = "data/outputs/test"
+    splatting_repo_dir: str = "gaussian-splatting"
 
 
 class FramesConfig(Config):
@@ -50,7 +52,7 @@ def frames(
         # /bin/ffmpeg -i $FILE_NAME -qscale:v 1 -qmin 1 -vf fps=$FRAME_RATE {i}%04d.jpg
         for i, file in enumerate(video_files):
             _logger.info(f"Processing video file {file}")
-            subprocess.run(
+            result = subprocess.run(
                 [
                     "ffmpeg",
                     "-i",
@@ -62,8 +64,11 @@ def frames(
                     "-vf",
                     f"fps={config.frames_per_second}",
                     f"{tmp_dir}/{i}%04d.jpg",
-                ]
+                ],
+                capture_output=True,
             )
+            if result.returncode != 0:
+                raise ValueError(f"ffmpeg failed on file {file} with code {result.returncode}, and stderr:\n\n{result.stderr.decode()}")
 
         _logger.info("Copying images to output dir")
         # copy all images to output dir
@@ -77,8 +82,31 @@ def frames(
 
 
 @asset(deps=[frames])
-def point_cloud() -> None:
-    pass
+def point_cloud(
+    marshaller: Marshaller,
+    gaussian_splat_config: GaussianSplatConfig,
+) -> None:
+    # python convert.py -s $DATA_DIR
+
+    data_dir = marshaller.load_dir(gaussian_splat_config.root_dir)
+
+    # a better way to do this would be to use a python API
+    # but there isn't one for for this script, it's meant to be run from the command line
+    executable = sys.executable
+    script = str(Path(gaussian_splat_config.splatting_repo_dir) / "convert.py")
+    result = subprocess.run(
+        [executable, script, "-s", str(data_dir)],
+        stdout=sys.stdout,
+        stderr=subprocess.PIPE,
+    )
+
+    if result.returncode != 0:
+        raise ValueError(
+            f"convert.py failed with code {result.returncode} and stderr:\n\n{result.stderr.decode()}"
+        )
+
+    marshaller.upload_dir(data_dir, gaussian_splat_config.root_dir)
+
 
 
 @asset(deps=[point_cloud])
