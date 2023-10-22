5ba0f99c0260eab6ddee2f6d85c26c29e276f4ee
Jerome Swannack <jeromeswannack@gmail.com>
2023-10-22 15:04:15 +1300

    Run configuration PoC working

--- dagsplat/__init__.py
+++ dagsplat/__init__.py
@@ -1,9 +1,9 @@
 from dagster import Definitions
 
-from .assets import splat_job, frames, point_cloud, ply_file, splat_file
+from .assets import splat_job, frames, point_cloud, trained_ply_file, splat_file
 from .resources import RESOURCES
 
-all_assets = [frames, point_cloud, ply_file, splat_file]
+all_assets = [frames, point_cloud, trained_ply_file, splat_file]
 
 definitions = Definitions(
     assets=all_assets,
--- dagsplat/assets.py
+++ dagsplat/assets.py
@@ -1,28 +1,37 @@
-from dagster import asset, define_asset_job, get_dagster_logger
+from dagster import asset, define_asset_job, get_dagster_logger, Config
+
+from dagsplat.marshaller import Marshaller
 
 _logger = get_dagster_logger()
 
 
+class FramesConfig(Config):
+    frames_per_second: int = 6
+
+
 @asset
-def frames() -> str:
+def frames(
+    marshaller: Marshaller,
+    config: FramesConfig,
+) -> str:
     return "todo"
 
 
 @asset
-def point_cloud(test_asset: str) -> str:
-    _logger.info(test_asset)
+def point_cloud(frames: str) -> str:
+    _logger.info(frames)
     return "todo"
 
 
 @asset
-def ply_file(point_cloud: None) -> str:
+def trained_ply_file(point_cloud: str) -> str:
     _logger.info(point_cloud)
     return "todo"
 
 
 @asset
-def splat_file(ply_file: str) -> str:
-    _logger.info(ply_file)
+def splat_file(trained_ply_file: str) -> str:
+    _logger.info(trained_ply_file)
     return "todo"
 
 
@@ -31,16 +40,16 @@ splat_job = define_asset_job(
     selection=[
         frames,
         point_cloud,
-        ply_file,
+        trained_ply_file,
         splat_file,
     ],
-    config={
-        "execution": {
-            "config": {
-                "multiprocess": {
-                    "max_concurrent": 1,
-                }
-            }
-        }
-    },
+    # config={
+    #     "execution": {
+    #         "config": {
+    #             "multiprocess": {
+    #                 "max_concurrent": 1,
+    #             }
+    #         }
+    #     },
+    # },
 )
